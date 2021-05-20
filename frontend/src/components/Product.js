22dcbf7c1f546a756a45943e5d42c209d751a9db
futurelinker <hola@futurelink.me>
2021-05-19 22:54:41 -0500

    Rating Component and Router added

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,25 +1,30 @@
 import { Card, CardImg } from 'react-bootstrap'
+import { Link } from "react-router-dom";
+import Rating from './Rating'
 
 const Product = ( {product} ) => {
   return (
     <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <CardImg src={product.image} variant='top' />
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as='div'>
-              <strong>{product.name}</strong>
+            <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as='div'>
-            <div className='my-3'>
-                {product.rating} from {product.numReviews} reviews
-            </div>
+          <div className='my-3'>
+            {/* {product.rating} from {product.numReviews} reviews */}
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+              color='#f8e825'
+            />
+          </div>
         </Card.Text>
-        <Card.Text as='h3'>
-            ${product.price}
-        </Card.Text>
+        <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
     </Card>
   )
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,8 +7,8 @@ const HomeScreen = () => {
   return (
     <>
       <Row>
-        {products.map(( product ) => (
-          <Col sm={12} md={6} lg={4}>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4}>
             <Product product={product} />
           </Col>
         ))}
