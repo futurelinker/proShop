c7833551292bd1ca8018e58e79c8d5f5848579f2
futurelinker <hola@futurelink.me>
2021-05-20 22:43:56 -0500

    Basic express API backend setup

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const products = require('./data/products');
+
+const app = express()
+
+app.get('/api/products', (req, res,) => {
+    res.status(200).json(products)
+})
+
+app.get('/api/product/:id', (req, res,) => {
+    const product = products.find(x => x._id === req.params.id)
+    res.status(200).json(product)
+})
+
+
+app.listen(5000, console.log('server runing on port 5000'))
