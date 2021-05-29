74d9fc6e4cc617331191c807895474e0c5102f8e
futurelinker <hola@futurelink.me>
2021-05-28 23:57:20 -0500

    Cart screen added

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import { Row, Col, ListGroup, Image, Form, FormControl, Button, Card, ListGroupItem } from 'react-bootstrap'
 import { addToCart } from '../actions/cartActions'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
@@ -27,11 +27,61 @@ const CartScreen = ({ match, history, location }) => {
         }
     }, [ dispatch, productId, qty ])
 
+    const removeFromCartHandler = (id) => {
+        console.log('removed')
+    }
+
 
     return (
-        <div className='cart-screen'>
-            Cart
-        </div>
+      <div className='cart-screen'>
+        <Row>
+          <Col md={8}>
+            <h1>Shopping Cart</h1>
+            {cartItems.length === 0 ? (
+              <Message>
+                Your cart is empty <Link to='/'>Go back</Link>
+              </Message>
+            ) : (
+              <ListGroup variant='flush'>
+                {cartItems.map((item) => (
+                  <ListGroupItem key={item.product}>
+                    <Row>
+                      <Col md={2}>
+                        <Image src={item.image} alt={item.name} fluid rounded />
+                      </Col>
+                      <Col md={3}>
+                        <Link to={`/product/${item.product}`}>{item.name}</Link>
+                      </Col>
+                      <Col md={2}>{item.price}</Col>
+                      <Col md={2}>
+                        <FormControl
+                          as='select'
+                          value={qty}
+                          onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}
+                        >
+                          {/* [0,1,2,3, etc] */}
+                          {[...Array(item.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </FormControl>
+                      </Col>
+                      <Col md={2}>
+                          <Button variant='light' onClick={() => removeFromCartHandler(item.product)}>
+                              <i className='fas fa-trash'></i>
+                          </Button>
+                      </Col>
+                    </Row>
+                  </ListGroupItem>
+                ))}
+              </ListGroup>
+            )}
+          </Col>
+          <Col md={2}></Col>
+          <Col md={2}></Col>
+        </Row>
+      </div>
     )
 }
 
